1c8d5e8c69c47743d3ea7513200da7d7bbaef5ac
Abdallah Alqashqish <abdalq2003@gmail.com>
2024-11-06 11:21:42 -0500

    Add documentation to main code

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,23 +23,27 @@
 #include "entity/face/Face.hpp"
 #include "weights/MeshWeight.hpp"
 
-DebugFilter debug;
-PPMCapture capturer;
+DebugFilter debug; // The debug filter that renders shapes as lines
+PPMCapture capturer; // Captures the contents of the window as a PPM
 
 int main() {
 	try {
 		glfwInit();
 
+		// Create and startup window
 		Window window(512, 512, false, "Facial Expressions");
 		window.Launch();
 
+		// Input handler that processes clicks of registered keys
 		InputHandler inputHandler(window.getGLWindow());
 		window.ApplyCloseWindowToInputHandler(inputHandler);
 		debug.ApplyToInputHandler(inputHandler);
 		capturer.ApplyToInputHandler(inputHandler);
 
+		// Load shader program from the disk
 		ShaderProgram shaderProgram("resources/shaders/default.vert", "resources/shaders/default.frag");
 
+		// The face blend mesh
 		FaceMesh faceBlendMesh;
 		faceBlendMesh.GLInit();
 
@@ -51,26 +55,31 @@ int main() {
 			glm::vec3(0, 0, 0)
 		);
 
+		// Setup the camera
 		Camera camera(glm::vec3(20, 100, 200), 30.0f, 1.0f, 0.1f, 1000.0f);
 		camera.LookAt(glm::vec3(0, 110, 0));
 
+		// Background color of the window
 		Color backgroundColor(0.3, 0.4, 0.5, 1.0f);
 
+		// A counter for the blend mesh id [0. 11]
 		int counter = -1;
 
+		// Register incrementing counter on up key click
 		inputHandler.AddKeyCallback(GLFW_KEY_UP, false, [&counter, &faceBlendMesh] (GLFWwindow* window) {
 			counter = (counter + 1) % 12;
 			faceBlendMesh.BlendMesh(counter);
 			std::cout << "Current Blend ID: " << counter << std::endl;
 		});
 
+		// Register decrementing counter on down key click
 		inputHandler.AddKeyCallback(GLFW_KEY_DOWN, false, [&counter, &faceBlendMesh] (GLFWwindow* window) {
-			if(counter > 0) {
+			if(counter > 0) { // Is the blend mesh id one of the blend meshes and not the base mesh?
 				counter -= 1;
 				faceBlendMesh.BlendMesh(counter % 12);
 				
 				std::cout << "Current Blend ID: " << counter << std::endl;
-			} else if(counter == 0) {
+			} else if(counter == 0) { // Is the user going back to the face mesh?
 				counter = -1;
 				faceBlendMesh.Reset();
 				std::cout << "Current Blend is Base Mesh" << std::endl;
@@ -78,22 +87,33 @@ int main() {
 		});
 
 		while(!window.ShouldClose()) {
+			// Process user input into the window
 			inputHandler.ProcessInput();
 
+			// Set background color of the window
 			window.ClearColor(backgroundColor);
 
+			// Activate shader program
 			shaderProgram.Activate();
+			
+			// Send info to shader
 			debug.HandleDebugShader(shaderProgram);
 			camera.Apply(shaderProgram);
 
+			// Render the face
 			face.Render(shaderProgram);
 
+			// Swap back and front buffers
 			window.SwapBuffers();
 
+			// Poll keyboard input
 			glfwPollEvents();
 		}
 
+		// Delete face mesh VAO and VBO
 		faceBlendMesh.Delete();
+
+		// Delete shader program
 		shaderProgram.Delete();
 		glfwTerminate();
 
